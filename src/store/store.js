ffc5d6899661e4073bc11bc7fbd6a0cef6d2f3a7
Matthias Stahl <matthias.stahl87@web.de>
2019-01-11 00:30:16 +0100

    store bugfix

--- src/store/store.js
+++ src/store/store.js
@@ -7,21 +7,28 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   strict: true,
   state: {
-    token: null,
-    user: null,
-    isUserLoggedIn: false,
+    token: localStorage.getItem('user-token') || null,
+    user: JSON.parse(localStorage.getItem('user')) || null,
+    isUserLoggedIn: Boolean(localStorage.getItem('user-token')),
   },
   mutations: {
     setToken(state, token) {
       state.token = token;
       if (token) {
         state.isUserLoggedIn = true;
+        localStorage.setItem('user-token', token);
       } else {
         state.isUserLoggedIn = false;
+        localStorage.removeItem('user-token');
       }
     },
     setUser(state, user) {
       state.user = user;
+      if (user) {
+        localStorage.setItem('user', JSON.stringify(user));
+      } else {
+        localStorage.removeItem('user');
+      }
     },
   },
   actions: {
